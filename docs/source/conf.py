9c9ff0ede602a21407ed3697d007c476984f81cd
Felix Thelen <felix.thelen@ruhr-uni-bochum.de>
2024-11-25 09:36:50 +0100

    Tests for getting autodoc work (4)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path('..', 'src').resolve()))
+sys.path.insert(0, str(Path(__file__).parents[2] / 'src' / 'pysimtra'))
 
 print(sys.path)
 
